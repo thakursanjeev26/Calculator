6d4a384f680ed8738a14834f762b1d6b3eaac3cc
thakursanjeev26 <sanjeevjthakur@gmail.com>
2024-08-12 17:38:49 +0530

    Added:Additon Functionality is Added

--- src/com/bridgelabz/Calculator.java
+++ src/com/bridgelabz/Calculator.java
@@ -3,5 +3,8 @@ package com.bridgelabz;
 public class Calculator {
     public static void main(String[] args) {
         System.out.println("Welcome to Calculator");
+        int a=10,b=20,c;
+        c=a+b;
+        System.out.println("Addition is c"+c);
     }
 }
